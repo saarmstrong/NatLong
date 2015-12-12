c6d1a49091237e7e512b964e29c7b1ec3f8d1868
Richard Park <objectiv@gmail.com>
2015-12-12 00:47:37 -0500

    chore: stop importing from angular2/angular2
    Resolves #5

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
-import { provide } from 'angular2/angular2';
+import { bootstrap } from 'angular2/bootstrap';
+import { provide } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
-import { bootstrap } from 'angular2/platform/browser';
 import { APP_BASE_HREF, ROUTER_PROVIDERS } from 'angular2/router';
-import { AUTH_PROVIDERS } from './modules/auth/providers';
+
+// root component
 import { App } from './components/app/app';
-import 'styles/styles.scss';
+
+// modules
+import { AUTH_PROVIDERS } from './modules/auth/providers';
+
+// global styles
+import './styles/styles.scss';
 
 
 bootstrap(App, [
-  provide(APP_BASE_HREF, {useValue: '/'}),
   HTTP_PROVIDERS,
   ROUTER_PROVIDERS,
-  AUTH_PROVIDERS
+  AUTH_PROVIDERS,
+  provide(APP_BASE_HREF, {useValue: '/'})
 ]).catch((error: Error) => console.error(error));
